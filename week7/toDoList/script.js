58d375056ae717a883a252c1890c7ceabf3b3136
Todd Murphy <t.murphy@aerocare.com.au>
2017-06-03 12:02:41 +1000

    Finished to do list

--- week7/toDoList/script.js
+++ week7/toDoList/script.js
@@ -10,7 +10,45 @@ function addMyListItem() {
   // Create our new list item
   var item = document.createElement("li");
 
+  // Assign text from textbox to list item element
   item.innerText = myItemTextBox.value;
 
+  // Add strike through functionality
+  item.onclick = function() {
+    // If the item's text does not have line-through decoration
+    if (this.style.textDecoration !== "line-through") {
+      //Assign text decoration to line-through
+      this.style.textDecoration = "line-through";
+    } else {
+      //Assign text decoration to none
+      this.style.textDecoration = "none";
+    }
+  }
+
+  // Create list item remove button 
+
+  // Create new button element
+  var myRemoveButton = document.createElement("button");
+
+  // Create new button element
+  myRemoveButton.innerText = "-";
+
+  // Assign text to button
+
+  // Define onclick event callback for remove button
+  myRemoveButton.onclick = function() {
+    myList.removeChild(item);
+  }
+
+  // Add our remove button to the list item
+  item.appendChild(myRemoveButton);
+
+  // Add our list item to the un-ordered list
   myList.prepend(item);
+
+  // Clear textbox value by assigning an empty string
+  myItemTextBox.value = "";
+
+  // Put focus on our textbox so the user can enter new text without re-selecting the textbox
+  myItemTextBox.focus();
 }
