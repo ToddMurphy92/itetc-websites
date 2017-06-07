0ebc44f77599f0a4151f4e90ee341be047e36450
Todd Murphy <t.murphy@aerocare.com.au>
2017-06-07 22:13:37 +1000

    Work on challenge pre-alpha

---
+++ challenge/js/memoryGame.js
@@ -0,0 +1,43 @@
+
+//  Total number of attempts at matching pictures
+//  Is displayed at the end of each game
+var attempts = 0;
+
+// Array of images
+/** Placeholder - Have not added images yet
+var imgArray = [
+  images/rick.jpg,
+  images/doge.jpg,
+  images/harambe.jpg,
+  images/pepe.jpg,
+  images/etc.jpg
+];
+*/
+
+// Array of boxes
+var box [
+  "box1",
+  "box2",
+  "box3",
+  "box4",
+  "box5",
+  "box6",
+  "box7",
+  "box8",
+  "box9",
+  "box10",
+  "box11",
+  "box12"
+];
+
+// Play Again
+
+// Restart
+
+// Function - Shuffle
+
+// Optional Feature - Game Timer
+
+// Optional Feature - Turn Timer
+
+// Optional Feature - Local Two Player
